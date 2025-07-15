2b8b8b9c0c3b3eba888225edf44c6ae03c2a31d0
itsmenisha <90882851+itsmenisha@users.noreply.github.com>
2025-07-15 18:58:47 +0545

    Add files via upload

---
+++ js/loadhtml.js
@@ -0,0 +1,8 @@
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
